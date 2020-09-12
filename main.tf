aba6f3866f126be8042302d12bb00cdc179d74b5
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-12 16:59:24 +0900

    Chap3.6 Finish

--- main.tf
+++ main.tf
@@ -18,11 +18,40 @@ locals  {
   example_instance_type = "t3.micro"
 }
 
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = data.aws_ami.recent_amazon_linux_2.image_id
   instance_type = local.example_instance_type
+  vpc_security_group_ids = [
+    aws_security_group.example_ec2.id
+  ]
+  user_data = <<EOF
+  #!/bin/bash
+  yum install -y httpd
+  systemctl start httpd.service
+  EOF
 }
 
 output "example_instance_id" {
   value = aws_instance.example.id
+}
+
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
 }
