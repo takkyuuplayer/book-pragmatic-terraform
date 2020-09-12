afde3c182d7b1f792fb32136f5e085e4391d8a8d
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-12 16:50:58 +0900

    Chap3.5 Finish

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+  filter {
+    name = "state"
+    values = ["available"]
+  }
+}
 locals  {
   example_instance_type = "t3.micro"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-064c81ce3a290fde1"
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id
   instance_type = local.example_instance_type
 }
 
