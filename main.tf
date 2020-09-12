b6570782ed47a12bff6c734d38320b68154c15b8
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-12 17:03:28 +0900

    Chap3.7 Finish

--- main.tf
+++ main.tf
@@ -41,11 +41,7 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = [
     aws_security_group.example_ec2.id
   ]
-  user_data = <<EOF
-  #!/bin/bash
-  yum install -y httpd
-  systemctl start httpd.service
-  EOF
+  user_data = file("./user_data.sh")
 }
 
 output "example_instance_id" {
