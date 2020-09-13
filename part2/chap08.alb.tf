f855f194bde92eca8c40a2776c712b14cbd5efba
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-13 13:02:09 +0900

    Chap8.6 Finish

--- part2/chap08.alb.tf
+++ part2/chap08.alb.tf
@@ -62,37 +62,75 @@ resource "aws_lb_listener" "http" {
     }
 }
 
-# # HTTPS
+# Chap8.5 HTTPS ALB
 
-# resource "aws_lb_listener" "https" {
-#     load_balancer_arn = aws_lb.example.arn
-#     port = 443
-#     protocol = "HTTPS"
-#     certificate_arn = aws_acm_certificate.example.arn
-#     ssl_policy = "ELBSecurityPolicy-2016-08"
+resource "aws_lb_listener" "https" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 443
+    protocol = "HTTPS"
+    certificate_arn = aws_acm_certificate.example.arn
+    ssl_policy = "ELBSecurityPolicy-2016-08"
 
-#     default_action {
-#         type = "fixed-response"
+    default_action {
+        type = "fixed-response"
 
-#         fixed_response {
-#             content_type = "text/plain"
-#             message_body = "This is HTTPS"
-#             status_code = "200"
-#         }
-#     }
-# }
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "This is HTTPS"
+            status_code = "200"
+        }
+    }
+}
 
-# # HTTP redirect
-# resource "aws_lb_listener" "redirect_http_to_https" {
-#     load_balancer_arn = aws_lb.example.arn
-#     port = "8080"
-#     protocol = "HTTP"
-#     default_action {
-#         type = "redirect"
-#         redirect {
-#             port = "443"
-#             protocol = "HTTPS"
-#             status_code = "HTTP_301"
-#         }
-#     }
-# }
+# HTTP redirect
+resource "aws_lb_listener" "redirect_http_to_https" {
+    load_balancer_arn = aws_lb.example.arn
+    port = "8080"
+    protocol = "HTTP"
+    default_action {
+        type = "redirect"
+        redirect {
+            port = "443"
+            protocol = "HTTPS"
+            status_code = "HTTP_301"
+        }
+    }
+}
+
+# Chap8.6 Request Forwareding
+resource "aws_lb_target_group" "example" {
+    name = "example"
+    target_type = "ip"
+    vpc_id = aws_vpc.example.id
+    port = 80
+    protocol = "HTTP"
+    deregistration_delay = 300
+
+    health_check {
+        path = "/"
+        healthy_threshold = 5
+        unhealthy_threshold = 2
+        timeout = 5
+        interval = 30
+        matcher = 200
+        port = "traffic-port"
+        protocol = "HTTP"
+    }
+
+    depends_on = [aws_lb.example]
+}
+
+resource "aws_lb_listener_rule" "example" {
+    listener_arn = aws_lb_listener.https.arn
+    priority = 100
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.example.arn
+    }
+    condition {
+        path_pattern {
+            values = ["/*"]
+        }
+    }
+}
--- part2/chap08.route53.tf
+++ part2/chap08.route53.tf
@@ -18,26 +18,36 @@ output "domain_name" {
     value = aws_route53_record.example.name
 }
 
-# # SSL Certificate
-# resource "aws_acm_certificate" "example" {
-#     domain_name = aws_route53_record.example.name
-#     subject_alternative_names = []
-#     validation_method = "DNS"
+# Chap8.4 ACM
+resource "aws_acm_certificate" "example" {
+  domain_name               = data.aws_route53_zone.example.name
+  subject_alternative_names = []
+  validation_method         = "DNS"
 
-#     lifecycle {
-#         create_before_destroy = true
-#     }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_route53_record" "example_certificate" {
-#     name = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
-#     type = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
-#     records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
-#     zone_id = data.aws_route53_zone.example.id
-#     ttl = 60
-# }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-3-upgrade#resource-aws_acm_certificate
+resource "aws_route53_record" "example_certificate" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-# resource "aws_acm_certificate_validation" "example" {
-#     certificate_arn = aws_acm_certificate.example.arn
-#     validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [for record in aws_route53_record.example_certificate : record.fqdn]
+}
