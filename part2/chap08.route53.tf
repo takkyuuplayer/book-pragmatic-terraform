4af1bcf8db69d7bdaac7cbfb71ebababec2d26ee
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-13 11:19:23 +0900

    Chap8.3 Route53

---
+++ part2/chap08.route53.tf
@@ -0,0 +1,43 @@
+data "aws_route53_zone" "example" {
+    name = "takkyuuplayer.com"
+}
+
+resource "aws_route53_record" "example" {
+    zone_id = data.aws_route53_zone.example.zone_id
+    name = "terraform.${data.aws_route53_zone.example.name}"
+    type = "A"
+
+    alias {
+        name = aws_lb.example.dns_name
+        zone_id = aws_lb.example.zone_id
+        evaluate_target_health = true
+    }
+}
+
+output "domain_name" {
+    value = aws_route53_record.example.name
+}
+
+# # SSL Certificate
+# resource "aws_acm_certificate" "example" {
+#     domain_name = aws_route53_record.example.name
+#     subject_alternative_names = []
+#     validation_method = "DNS"
+
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+# }
+
+# resource "aws_route53_record" "example_certificate" {
+#     name = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+#     type = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+#     records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+#     zone_id = data.aws_route53_zone.example.id
+#     ttl = 60
+# }
+
+# resource "aws_acm_certificate_validation" "example" {
+#     certificate_arn = aws_acm_certificate.example.arn
+#     validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+# }
