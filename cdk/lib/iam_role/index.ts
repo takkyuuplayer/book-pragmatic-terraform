dc6c310df64fd74d11bc368c3b4ffa9adb166ead
Takafumi SEKIGUCHI <takkyuuplayer@gmail.com>
2021-01-12 00:15:06 +0900

    Chap 5 権限管理 (#3)

---
+++ cdk/lib/iam_role/index.ts
@@ -0,0 +1,26 @@
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import { FORMERR } from "dns";
+import { Policy, Role } from "@aws-cdk/aws-iam";
+
+interface IamRoleProps extends cdk.NestedStackProps {
+  name: string;
+  policy: iam.PolicyDocument;
+  identifier: string;
+}
+
+export function IamRoleStack(scope: cdk.Construct, id: string, props: IamRoleProps): {
+    role: Role,
+    policy: Policy,
+} {
+    const role = new iam.Role(scope, `${id}Role`, {
+      assumedBy: new iam.ServicePrincipal(props.identifier),
+    })
+
+    const policy = new iam.Policy(scope, `${id}Policy`, {
+      document: props.policy,
+    });
+    policy.attachToRole(role)
+
+    return { role, policy }
+}
