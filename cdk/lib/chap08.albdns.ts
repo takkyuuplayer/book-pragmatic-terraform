bf156cb50bbb05aaab69d1beec74e88f5d7c1cd8
takkyuuplayer <takkyuuplayer@gmail.com>
2021-01-17 13:58:27 +0900

    Chap 8.4, 8.5 certificate and https

--- cdk/lib/chap08.albdns.ts
+++ cdk/lib/chap08.albdns.ts
@@ -1,3 +1,4 @@
+import * as acm from "@aws-cdk/aws-certificatemanager";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
 import { ListenerAction } from "@aws-cdk/aws-elasticloadbalancingv2";
@@ -38,6 +39,28 @@ export function AlbStack(scope: cdk.Construct, props: AlbProps): void {
     zone,
     recordName: `test.${zone.zoneName}`,
     target: RecordTarget.fromAlias(new LoadBalancerTarget(lb)),
+  });
+  const certificate = new acm.Certificate(scope, "Certificate", {
+    domainName: "test.takkyuuplayer.com",
+    validation: acm.CertificateValidation.fromDns(zone),
+  });
+  lb.addListener("https", {
+    port: 443,
+    protocol: elbv2.ApplicationProtocol.HTTPS,
+    certificates: [certificate],
+    defaultAction: ListenerAction.fixedResponse(200, {
+      contentType: "text/plain",
+      messageBody: "This is HTTPS",
+    }),
+  });
+  lb.addListener("redirect_http_to_https", {
+    port: 8080,
+    protocol: elbv2.ApplicationProtocol.HTTP,
+    defaultAction: ListenerAction.redirect({
+      port: "443",
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      permanent: true,
+    }),
   });
   new cdk.CfnOutput(scope, "DomainName", {
     exportName: "DomainName",
