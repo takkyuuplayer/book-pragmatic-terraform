a6f96cd39281fb66f203453ef5d06267a1b3b6f9
takkyuuplayer <takkyuuplayer@gmail.com>
2021-01-17 14:23:09 +0900

    Chap 8.6 Request forwarding

--- cdk/lib/chap08.albdns.ts
+++ cdk/lib/chap08.albdns.ts
@@ -1,11 +1,18 @@
 import * as acm from "@aws-cdk/aws-certificatemanager";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
-import { ListenerAction } from "@aws-cdk/aws-elasticloadbalancingv2";
+import {
+  ApplicationListenerRule,
+  ApplicationTargetGroup,
+  ListenerAction,
+  ListenerCondition,
+  TargetType,
+} from "@aws-cdk/aws-elasticloadbalancingv2";
 import { ARecord, HostedZone, RecordTarget } from "@aws-cdk/aws-route53";
 import { LoadBalancerTarget } from "@aws-cdk/aws-route53-targets";
 import { Bucket } from "@aws-cdk/aws-s3";
 import * as cdk from "@aws-cdk/core";
+import { Duration } from "@aws-cdk/core";
 
 interface AlbProps {
   vpc: ec2.Vpc;
@@ -44,14 +51,14 @@ export function AlbStack(scope: cdk.Construct, props: AlbProps): void {
     domainName: "test.takkyuuplayer.com",
     validation: acm.CertificateValidation.fromDns(zone),
   });
-  lb.addListener("https", {
+  const listener = lb.addListener("https", {
     port: 443,
     protocol: elbv2.ApplicationProtocol.HTTPS,
     certificates: [certificate],
-    defaultAction: ListenerAction.fixedResponse(200, {
-      contentType: "text/plain",
-      messageBody: "This is HTTPS",
-    }),
+    // defaultAction: ListenerAction.fixedResponse(200, {
+    //   contentType: "text/plain",
+    //   messageBody: "This is HTTPS",
+    // }),
   });
   lb.addListener("redirect_http_to_https", {
     port: 8080,
@@ -62,6 +69,24 @@ export function AlbStack(scope: cdk.Construct, props: AlbProps): void {
       permanent: true,
     }),
   });
+
+  const tg = new ApplicationTargetGroup(scope, "exampleTargetGroup", {
+    targetType: TargetType.IP,
+    vpc: props.vpc,
+    port: 80,
+    protocol: elbv2.ApplicationProtocol.HTTPS,
+    deregistrationDelay: Duration.seconds(300),
+  });
+  listener.addTargetGroups("example", {
+    targetGroups: [tg],
+  });
+  new ApplicationListenerRule(scope, "exampleApplicationListenerRule", {
+    listener,
+    priority: 100,
+    action: ListenerAction.forward([tg], {}),
+    conditions: [ListenerCondition.pathPatterns(["/*"])],
+  });
+
   new cdk.CfnOutput(scope, "DomainName", {
     exportName: "DomainName",
     value: rec.domainName,
