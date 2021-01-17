995c6d994d935ff2c5e46cb080622b0bfcdeeb9d
takkyuuplayer <takkyuuplayer@gmail.com>
2021-01-17 10:57:57 +0900

    Refactoring

--- cdk/lib/security_group/index.ts
+++ cdk/lib/security_group/index.ts
@@ -1,22 +1,26 @@
-import { Construct, Tags } from "@aws-cdk/core";
-import * as ec2 from "@aws-cdk/aws-ec2"
+import * as ec2 from "@aws-cdk/aws-ec2";
+import { Construct } from "@aws-cdk/core";
 
 interface SecurityGroupProps {
-    name: string
-    vpc: ec2.Vpc,
-    port: number,
-    cidrBlocks: Array<string>,
+  name: string;
+  vpc: ec2.Vpc;
+  port: number;
+  cidrBlocks: Array<string>;
 }
 
-export function SecurityGroupStack(scope: Construct, id: string, props: SecurityGroupProps) : ec2.SecurityGroup {
-    const sg = new ec2.SecurityGroup(scope, id, {
-        allowAllOutbound: true,
-        securityGroupName: props.name,
-        vpc: props.vpc,
-    })
-    props.cidrBlocks.forEach((cidrBlock) => {
-        sg.addIngressRule(ec2.Peer.ipv4(cidrBlock), ec2.Port.tcp(props.port))
-    })
+export function SecurityGroupStack(
+  scope: Construct,
+  id: string,
+  props: SecurityGroupProps
+): ec2.SecurityGroup {
+  const sg = new ec2.SecurityGroup(scope, id, {
+    allowAllOutbound: true,
+    securityGroupName: props.name,
+    vpc: props.vpc,
+  });
+  props.cidrBlocks.forEach((cidrBlock) => {
+    sg.addIngressRule(ec2.Peer.ipv4(cidrBlock), ec2.Port.tcp(props.port));
+  });
 
-    return sg
+  return sg;
 }
